cc608ab1477908d5649318cf0d9f352ecc1db59a
amandamaduenocasado <amandacaso97@gmail.com>
2024-11-08 17:47:15 +0100

    update!

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// RECUERDA: SE USA PARA ACCEDER O MODIFICAR ESTILOS EN LÍNEA 
+// HORA DE LLAMAR A LAS VARIABLES CSS
+const rootStyles = document.documentElement.style;
+
+// HORA DE LLAMAR A LAS CONSTANTES 
+
+const cvcCardElement = document.getElementById("cvcCard");
+const cardnumberCardElement = document.getElementById("cardnumberCard");
+const cardnameCardElement = document.getElementById("cardnameCard");
+const expdateCardElement = document.getElementById("expdateCard");
+const expyearCardElement = document.getElementById("expyearCard");
